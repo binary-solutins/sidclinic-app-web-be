0539519b26b03d19f7ffd07cde0ec911700f6393
satweekg <satweekg.shivinfotech@gmail.com>
2025-03-27 11:21:49 +0530

    swagger update

--- swagger.js
+++ swagger.js
@@ -15,7 +15,7 @@ const options = {
     },
     servers: [
       { url: 'http://localhost:3000/api', description: 'Development server' },
-      { url: 'https://api.sidclinic.com', description: 'Production server' }
+      { url: 'https://sidclinic-app-web-be.onrender.com/api', description: 'Testing Server' }
     ],
     components: {
       securitySchemes: {
